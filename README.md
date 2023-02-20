52d6af76b58b67f30a55c6df3e6e2bb6f920c0bb
parv-1 <philipparvaneh@gmail.com>
2023-02-20 15:41:13 -0500

    hw0

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # DS-hw0
+
+Google Drive: https://drive.google.com/drive/folders/1609m49TbsVMcBiiyjXvLL-2UYCCwQimo
+
+Outputs will be found in 'outputs.zip' which contains outputWordCount (step4), outputBigramCount, and outputBigramFreq 
+
+Step four: wordcount.py
+
+Step five: bigrams.py
--- bigramsFreq.py
+++ bigramsFreq.py
@@ -14,18 +14,11 @@ sc = SparkContext(conf=conf)
 		
 words = sc.textFile(sys.argv[1])
 
-wordCounts = words.flatMap(lambda line: line.split(" "))
-wordCounts = wordCounts.map(lambda word: (word, 1)).reduceByKey(lambda a,b: a+b)
-
 bigrams = words.map(lambda line: line.split(" "))
 bigrams = bigrams.flatMap(lambda word: [((word[i], word[i+1]),1) for i in range(0, len(word)-1)])
 
 bgCounts = bigrams.reduceByKey(lambda x,y: x+y)
-bgCounts = bgCounts.map(lambda x: (x[0][0], (x[0][1], x[1])))
 
-final = bgCounts.join(wordCounts)
-final = final.map(lambda x: ((x[0], x[1][0][0]), x[1][0][1]/x[1][1]))
-
-final.coalesce(1, shuffle=True).saveAsTextFile(sys.argv[2])
+bgCounts.coalesce(1, shuffle=True).saveAsTextFile(sys.argv[2])
 
 sc.stop()
